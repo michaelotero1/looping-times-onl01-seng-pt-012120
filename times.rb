96b112d128e8ee21ea2e13e6fa9dd36cd29a4bc0
michael otero <michaelotero1118@gmail.com>
2020-01-30 04:43:17 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,15 @@
-def using_times
-	#your code here
-end
+#def using_times("Wingardium Leviosa")
+  #7.times do
+    #puts using_times
+#end 
 
+using_times = "Wingardium Leviosa"
+
+def using_times
+7.times do 
+  puts "Wingardium Leviosa"
+end
+end
 
 
 
